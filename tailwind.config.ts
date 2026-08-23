862703f0f73848ee84de40189ca1d6d0aa09b571
vicisssyntrx <vicisssyntrx@gmail.com>
2026-08-23 16:01:04 +0530

    style: hardcode Apple fonts globally

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,19 @@ export default {
       },
     },
     extend: {
+      fontFamily: {
+        sans: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"SF Pro Display"',
+          '"SF Pro Text"',
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          "sans-serif",
+        ],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
